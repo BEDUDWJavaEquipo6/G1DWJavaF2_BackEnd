21e6d2fbb42870076e56b93f2066f9562cee7197
FrankTII <frank.training2@gmail.com>
2023-03-08 01:37:21 -0600

    Upload Postwork4 initial files
    build.gradle has code to run source and target code with jdkv 1.8 and Springboot v2.7.9, No reinstalation required so far

---
+++ postwork4/src/main/java/org/bedu/java/backend/postwork4/Postwork4Application.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.postwork4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Postwork4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Postwork4Application.class, args);
+	}
+
+}
---
+++ postwork4/src/main/java/org/bedu/java/backend/postwork4/model/Persona.java
@@ -0,0 +1,58 @@
+package org.bedu.java.backend.postwork4.model;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String email;
+    private String telefono;
+    private String direccion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
