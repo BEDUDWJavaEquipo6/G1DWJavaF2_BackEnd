308c0d46f505417b038726167a9c9ca1b05f0f73
FrankTII <frank.training2@gmail.com>
2023-03-08 01:41:35 -0600

    Add id attribute to Persona Class

--- postwork4/src/main/java/org/bedu/java/backend/postwork4/model/Persona.java
+++ postwork4/src/main/java/org/bedu/java/backend/postwork4/model/Persona.java
@@ -8,6 +8,16 @@ public class Persona {
     private String telefono;
     private String direccion;
 
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getNombre() {
         return nombre;
     }
