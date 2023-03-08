2d5c79902c998980df9bae22e1b535e249c42d4b
FrankTII <frank.training2@gmail.com>
2023-03-08 02:28:48 -0600

    Un println corriendo

--- postwork4/src/main/java/org/bedu/java/backend/postwork4/Postwork4Application.java
+++ postwork4/src/main/java/org/bedu/java/backend/postwork4/Postwork4Application.java
@@ -1,13 +1,19 @@
 package org.bedu.java.backend.postwork4;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Postwork4Application {
+public class Postwork4Application implements CommandLineRunner{
 
 	public static void main(String[] args) {
-		SpringApplication.run(Postwork4Application.class, args);
-	}
 
+		SpringApplication.run(Postwork4Application.class, args
+		);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Here the command line runner is running inside the spring boot ,,..//");
+	}
 }
