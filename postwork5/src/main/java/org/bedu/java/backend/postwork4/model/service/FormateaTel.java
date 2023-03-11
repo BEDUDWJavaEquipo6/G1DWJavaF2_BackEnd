bf2a48b13b62c59f1120a96bdbee5ccb59883afd
Rodrigo Aguilar <rodrigo_agrs@hotmail.com>
2023-03-11 09:44:14 -0600

    Crear clase formateo

--- postwork5/src/main/java/org/bedu/java/backend/postwork4/model/service/FormateaTel.java
+++ postwork5/src/main/java/org/bedu/java/backend/postwork4/model/service/FormateaTel.java
@@ -2,8 +2,8 @@ package org.bedu.java.backend.postwork4.model.service;
 /*
 public class FormateaTel {
     public String formatear(String telefono){
-
-    }
+        return telefono.replace(/(\d{2})(\d{4})(\d{4})/, "($1) $-2-$3");
+     }
 }
 
  */
