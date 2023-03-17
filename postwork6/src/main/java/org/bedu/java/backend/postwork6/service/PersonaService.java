89429ef05628aad517479ffaeeb87196a6c16e55
FrankTII <frank.training2@gmail.com>
2023-03-16 23:38:49 -0600

    Postwork6 updated implementation of PersonaService
    Now it sorts personas Set alphabetically by name

--- postwork6/src/main/java/org/bedu/java/backend/postwork6/service/PersonaService.java
+++ postwork6/src/main/java/org/bedu/java/backend/postwork6/service/PersonaService.java
@@ -1,11 +1,12 @@
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonaService {
-    private Set<Persona> personas = new HashSet<>();
+    private Set<Persona> personas = new TreeSet<>(Comparator.comparing(Persona::getName));
 
     public void addPersona(Persona persona) {
         personas.add(persona);
